bba756010147a873e097f759d5f7782c1a5b3660
Usman Ali <91782435+Uxmanmirza@users.noreply.github.com>
2022-01-06 17:40:25 +0500

    chaining

--- script.js
+++ script.js
@@ -91,6 +91,13 @@ const createUsernames = function (accs) {
 };
 createUsernames(accounts);
 console.log(accounts);
+
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance}€`;
+};
+
+calcDisplayBalance(account1.movements);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
