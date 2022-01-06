80f6e2cb7f90e518b90bf73cc3488cc0e15d528a
Usman Ali <91782435+Uxmanmirza@users.noreply.github.com>
2022-01-06 13:40:21 +0500

    usename creation

--- script.js
+++ script.js
@@ -79,6 +79,26 @@ const displayMovements = function (movements) {
   });
 };
 displayMovements(account1.movements);
+
+// const user = 'Steven Thomas Williams';
+// const username = user.split('').map(function (name) {
+//   return name[(0, 0, 0)];
+// });
+// //   .join('');
+
+// console.log(username);
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+createUsernames(accounts);
+console.log(accounts);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
